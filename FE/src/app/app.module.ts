6287a1f373842356b68d2c2735a37ae5fbc5bd82
DawnBreaker <tunganhngo207@gmail.com>
2025-01-03 16:28:49 +0700

    feat: update asssets & add view to FE

--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -6,10 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductService } from './services/product.service';
-import { ProductListTableComponent } from './components/product-list-table/product-list-table.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ProductListTableComponent],
+  declarations: [AppComponent, ProductListComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [ProductService],
   bootstrap: [AppComponent],
