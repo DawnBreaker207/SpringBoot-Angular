93185b8c9713f07e3684c60bb5f4c119fb7b5c1f
DawnBreaker <tunganhngo207@gmail.com>
2025-01-07 15:46:16 +0700

    feat: add ssl in FE

--- FE/src/app/config/my-app-config.ts
+++ FE/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oame5hl4ed1l9gSz5d7',
     issuer: 'https://dev-23616947.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
