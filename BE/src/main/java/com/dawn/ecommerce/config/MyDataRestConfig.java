6a3a1efcd9144ec172176577c31b9bf7a38e28c0
DawnBreaker <tunganhngo207@gmail.com>
2025-01-07 10:56:02 +0700

    feat: refactoring BE

--- BE/src/main/java/com/dawn/ecommerce/config/MyDataRestConfig.java
+++ BE/src/main/java/com/dawn/ecommerce/config/MyDataRestConfig.java
@@ -8,11 +8,13 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.util.pattern.PathPattern;
 
 import com.dawn.ecommerce.entity.Country;
 import com.dawn.ecommerce.entity.Product;
@@ -21,7 +23,8 @@ import com.dawn.ecommerce.entity.State;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
-
+    @Value("${allowed.origins}")
+    private String[] allowedOrigins;
     private EntityManager entityManager;
 
     @Autowired
@@ -32,7 +35,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
-	HttpMethod[] theUnSupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
+	HttpMethod[] theUnSupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH };
 
 	// Disable HTTP methods for Product: PUT, POST and DELETE
 	disableHttpMethods(Product.class, config, theUnSupportedActions);
@@ -43,6 +46,9 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
 	// Call an internal helper method
 	exposeIds(config);
+
+	// Configure CORS mapping
+	cors.addMapping(config.getBasePath() + "/**").allowedOrigins(allowedOrigins);
     }
 
     private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config,
--- FE/src/app/config/my-app-config.ts
+++ FE/src/app/config/my-app-config.ts
@@ -1,6 +1,6 @@
 export default {
   oidc: {
-    clientId: '0oamdpgiigeIWRF3s5d7',
+    clientId: '0oame5hl4ed1l9gSz5d7',
     issuer: 'https://dev-23616947.okta.com/oauth2/default',
     redirectUri: 'http://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
